815c25181d48f581aa76b0180e5aecfebcf9321e
rm72468 <gabrielltr84@gmail.com>
2015-10-13 21:05:17 -0300

    finalizado

--- app/src/main/java/br/com/fiap/rm72468/orcamento/MainActivity.java
+++ app/src/main/java/br/com/fiap/rm72468/orcamento/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 
 public class MainActivity extends Activity {
@@ -19,7 +22,7 @@ public class MainActivity extends Activity {
     EditText txtValordaNota;
     EditText txtCarga;
 
-    TextView txtValorTotaldoFrete;
+    TextView lblValorTotaldoFrete;
 
     Spinner spnEstado;
     Spinner spnCodigoCarga;
@@ -29,24 +32,28 @@ public class MainActivity extends Activity {
     Button btnCalcularFrete;
 
     String[] estado = {"Rio de Janeiro", "Minas Gerais", "Paraná", "São Paulo"};
-    String[] cargas = {"1","2","3","4","5","6","7","8","9","10",
-            "11","12","13","14","15","16","17","18","19","20",
-            "31","32","33","34","35","36","37","38","39","40"};
+    String[] codigoCargas = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         txtCliente = (EditText) findViewById(R.id.txtCliente);
         txtValordaNota = (EditText) findViewById(R.id.txtValordaNota);
         txtCarga = (EditText) findViewById(R.id.txtCarga);
-        txtValorTotaldoFrete = (TextView) findViewById(R.id.txtValorTotaldoFrete);
 
+        lblValorTotaldoFrete = (TextView) findViewById(R.id.lblValorTotaldoFrete);
+
+        // preeenche spinner Estado
         spnEstado = (Spinner) findViewById(R.id.spnEstado);
         spnEstado.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, estado));
 
+        // preeenche spinner Codigo da Carga
         spnCodigoCarga = (Spinner) findViewById(R.id.spnCodigoCarga);
-        spnCodigoCarga.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cargas));
+        spnCodigoCarga.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, codigoCargas));
 
         rgIncluirSeguro = (RadioGroup) findViewById(R.id.rgIncluirSeguro);
 
@@ -60,8 +67,52 @@ public class MainActivity extends Activity {
 
     }
 
-    private void calcularFrete() {
+    private double getDoubleTextView(TextView v) {
+        try {
+            return Double.parseDouble(String.valueOf(v.getText()));
+        } catch (Exception e) {
+            return 0.0;
+        }
+    }
 
+    private void calcularFrete() {
+        double valorNota = getDoubleTextView(txtValordaNota);
+        double carga = getDoubleTextView(txtCarga);
+        double valorFrete;
+        double imposto = 0;
+        String estadoSelecionado = spnEstado.getSelectedItem().toString();
+        switch (estadoSelecionado) {
+            case "Rio de Janeiro":
+                imposto = 0.25 * valorNota;
+                break;
+            case "Minas Gerais":
+                imposto = 0.20 * valorNota;
+                break;
+            case "Paraná":
+                imposto = 0.15 * valorNota;
+                break;
+            case "São Paulo":
+                imposto = 0.12 * valorNota;
+                break;
+        }
+        double pesoCarga = 0;
+        int codigoCargaSelecionado = Integer.parseInt(String.valueOf(spnCodigoCarga.getSelectedItem()));
+        if (codigoCargaSelecionado >= 1 && codigoCargaSelecionado <= 10)
+            pesoCarga = carga * 120;
+        else if (codigoCargaSelecionado >= 11 && codigoCargaSelecionado <= 20)
+            pesoCarga = carga * 200;
+        else if (codigoCargaSelecionado >= 31 && codigoCargaSelecionado <= 40)
+            pesoCarga = carga * 280;
+
+        // Frete = Imposto_Sobre_Nota + Código_da_Carga*Preço_Por_Quilo
+        valorFrete = imposto + pesoCarga;
+
+        if ((rgIncluirSeguro.getCheckedRadioButtonId()) == R.id.rbSim) {
+            valorFrete += valorNota * 0.1;
+        }
+
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        lblValorTotaldoFrete.setText(nf.format(valorFrete));
     }
 
 
